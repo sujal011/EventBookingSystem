bf397afc8c161f100255ac9d3e6a02fc065c3d00
Sujal Bhavsar <sujalofficial011@gmail.com>
2025-11-15 17:30:42 +0530

    server dockerfile works now

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
 	format: "esm",
 	outDir: "./dist",
 	clean: true,
-	noExternal: [/@EventBookingSystem\/.*/],
+	bundle: true,
+	// Bundle all dependencies for Docker
 });
